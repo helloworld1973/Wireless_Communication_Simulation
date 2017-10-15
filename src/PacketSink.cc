756573f7f7f933d48ff223d7c7f272129e36ad84
Jason Ye <yxzhjason@gmail.com>
2017-10-16 00:26:27 +1300

    ntdll!RtlEnterCriticalSection() from C:\WINDOWS\SYSTEM32\ntdll.dll

--- src/PacketSink.cc
+++ src/PacketSink.cc
@@ -13,17 +13,14 @@ class PacketSink : public cSimpleModule
 public:
     PacketSink();
     ~PacketSink();
-    int numOfPacketsReceived;
+
 protected:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
 
     std::string fileName;
-    //std::deque<AppMessage *> macBuffer;
-    AppMessage *sinkBuffer[10000];
-    int writeIndex;
-    int circBuffSize;
-
+    int numOfPackets;
+    FILE * filePointer;
 };
 Define_Module(PacketSink);
 
@@ -33,85 +30,39 @@ PacketSink::PacketSink()
 }
 
 PacketSink::~PacketSink()
-{
-
-    //Number of Packets transmitted VS number of Packets received
-    //PacketSink fileName doesnt make sense to be
-    //used here
-    //std::string fileName = "Data_Received.txt";
-    FILE * filePointerToWrite = fopen("Data_Received.txt", "a");
-    if (filePointerToWrite == NULL) return;
-
-    // retrieve node position in the network (from parents)
-    int nodeXPosition = getParentModule()->par("nodeXPosition");
-    int nodeYPosition = getParentModule()->par("nodeYPosition");
-
-    // retrieve node identifier from parent
-    int nodeIdentifier = getParentModule()->par("nodeIdentifier");
-
-
-    fprintf(filePointerToWrite, "ReceiverNode #           NumOfMessage Received         Position(X.Y)\n");
-    fprintf(filePointerToWrite, "%d,                      %d,                           %d,%d\n",
-            nodeIdentifier, numOfPacketsReceived, nodeXPosition, nodeYPosition);
-
-    fclose(filePointerToWrite);
-
-    //Start writing our message into log files
-    FILE * MessageLogFilePointer = fopen("Message_PacketSink.txt", "a");
-
-    for(int i = 0; i < numOfPacketsReceived; i++){
-        if(numOfPacketsReceived >= circBuffSize) break;
-        AppMessage *appMsg = sinkBuffer[i];
-        //we can perfect the formatting later...
-            //fprintf(MessageLogFilePointer, "%d,                  %d,       %f,        %d,        %d\n",
-                   //numOfPacketsReceived, appMsg->msgSize, SIMTIME_DBL(appMsg->timeStamp), appMsg->senderId, appMsg->sequenceNumber);
-    }
-    fclose(MessageLogFilePointer);
-
-
+{  
+	 fclose(filePointer);     
 }
 
 void PacketSink::initialize()
 {
-    //save memory for heavy-loaded testing
-    circBuffSize = 10000;
-    writeIndex = 0;
-    numOfPacketsReceived = 0;
-    FILE * MessageLogFilePointer = fopen("Message_PacketSink.txt", "a");
-    if (MessageLogFilePointer != NULL)
+	fileName=par("fileName").str();
+	char* fileNameChar=(char*)fileName.data();
+    numOfPackets = 0;
+    filePointer = fopen(fileNameChar, "a");
+    if (filePointer != NULL)
     {
-
-            fprintf(MessageLogFilePointer, "NumOfMessage Received      msgSize      timeStamp      SenderID      sequenceNumber\n");
-            fclose(MessageLogFilePointer);
+        fprintf(filePointer, "Index      ReceivedTimeStamp      GeneratedTimeStamp      SenderID      sequenceNumber      msgSize\n");
+        //fclose(filePointer);
     }
-    // take parameters
-    fileName = par("fileName").str();
+    fclose(filePointer);
 }
+
 
 void PacketSink::handleMessage(cMessage *msg)
 {
-
-
-    if (dynamic_cast<AppMessage *>(msg))
+    if (check_and_cast<AppMessage *>(msg))
     {
-        //File pointer CAN NOT be handled here
-        AppMessage *appMsg = static_cast<AppMessage *>(msg);
-
-        if (writeIndex < circBuffSize){
-            sinkBuffer[writeIndex] = appMsg;
-        }
-        else{
-            //Circulate back to the start
-            writeIndex = 0;
-            }
-
-        writeIndex++;
+    	AppMessage *appMsg = static_cast<AppMessage *>(msg);
+    	//simtime_t timeStamp = simTime();//current simulation time
+    	char* fileNameChar=(char*)fileName.data();
+    	filePointer = fopen(fileNameChar, "a");
+    	fprintf(filePointer, "%d,     %d,      %d,      %d\n",
+             numOfPackets,  appMsg->getSenderId(), appMsg->getSequenceNumber(), appMsg->getMsgSize());
+                                          //change (simtime_t) to (double)
+        numOfPackets++;
         delete msg;
-    }
+        fclose(filePointer);
 
-    else
-    {
-        //It should never reach here
-        delete msg;
     }
 }
